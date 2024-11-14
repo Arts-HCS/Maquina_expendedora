c538e6389f6adf7313411a452ae3f31d41dc30ed
aRtz <159662306+Arts-HCS@users.noreply.github.com>
2024-11-14 14:43:17 -0600

    Add files via upload

--- practica9- vending mach/main.py
+++ practica9- vending mach/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, request
+import random
 
 app = Flask(__name__)
 
 display = "" 
 products = []
-unidades = 1
+unidades = [random.randint(1,10) for i in range(41)]
 
 @app.route('/', methods=["POST", "GET"])
 def index():
@@ -23,18 +24,21 @@ def upload():
                     display = ""
                     return render_template('index.html', error2=error2, products=products, unidades=unidades)
                 name = "drink"+display
-                if products.count(name) >=unidades: 
+                if products.count(name) >=unidades[int(display)]: 
                     error = "No hay más unidades"
                     display = ""
                     return render_template('index.html', error=error, products=products, unidades=unidades)
                 products.append(name)
                 drink_value = request.form[name]
-                display = ""
                 return render_template('index.html', drink_value=drink_value, products=products, unidades=unidades)
         elif value == "fproduct":
+            nam = 'drink'+display
+            diferencia = unidades[int(display)] - products.count(nam)
             display = ""
-            mensaje = "Tomaste un producto"    
-            return render_template('index.html', products=products, unidades=unidades, mensaje=mensaje)    
+            mensaje1 = "Tomaste un producto"
+            mensaje2 = "Quedan "+ str(diferencia)+" unidades"
+            print(unidades)    
+            return render_template('index.html', products=products, unidades=unidades, mensaje1=mensaje1, mensaje2=mensaje2)    
         elif value == "C":
             display = ""
         elif value == "R":
